091864730ff3af53c7ae66ffcc70e96185cdc21d
João Victor Cabral de Melo (NightOff) <joao.victor.cabralmelo@gmail.com>
2018-02-08 21:01:08 -0200

    Add 1059

---
+++ main.java
@@ -0,0 +1,17 @@
+
+import  static java.lang.System.out;
+
+
+public class main {
+
+	public static void Pares(String[] args) {
+		int i;
+		
+		for(i = 1; i <= 100;i++) {
+			if(i % 2 == 0) {
+				out.println(i);
+			}
+		}
+	}
+
+}
